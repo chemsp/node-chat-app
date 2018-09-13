f38d4cb78533672c1a40f31ffd9491c4cb10e058
Sunil <chemsp@gmail.com>
2018-09-13 10:48:32 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+const  publicPath = path.join(__dirname,'/../public');
+const  app = express();
+ process.env.PORT = 3000;
+
+    
+
+app.use(express.static(publicPath)); 
+
+    
+
+
+    app.listen(3000);
