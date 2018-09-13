67c6ef79c8a0260d48d29f44fd0deeb84404806f
Sunil <chemsp@gmail.com>
2018-09-13 15:42:54 -0400

    emit message on create new message

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+var socket  = io();
+socket.on('connect',function(){
+//   console.log('Conntected to Server');
+
+//   socket.emit('createEmail',{
+//     to:"margi@gmail.com",
+//     text :"Happy Birthday",
+//     createdAt : 12343
+//   });
+
+socket.emit('createMessage',{
+  from:"client",
+  text: 'Hello Everyone',
+  createdAt :'12344'
+});
+
+socket.on('newMessage',function(message){
+  console.log('newMessage',message);
+  
+});
+
+// socket.on('disconnect',function(){
+//   console.log('Disconnected from  Server');
+
+// });
+
+socket.on('newEmail',function(email){
+  console.log('New Mail Received',email);
+});
+
+
+});
--- server/server.js
+++ server/server.js
@@ -1,15 +1,62 @@
 const path = require('path');
 
 const express = require('express');
+const socketIO = require("socket.io");
+const http = require('http');
 const  publicPath = path.join(__dirname,'/../public');
 const  app = express();
  const port = process.env.PORT || 3000;
+ 
+
+ var server = http.createServer(app);
+
+ var io = socketIO(server);
+   io.on('connection',(socket)=>{
+       console.log('New User Connnected');
+
+    //    socket.on('createEmail',function(email){
+    //     console.log('New email Created', email);
+    // });
+  
+      socket.on('createMessage',function(mssge){
+         // console.log(mssge);
+         io.emit('newMessage',{
+            from: mssge.from,
+            text : mssge.text,
+            createdAt : new Date().getTime()
+                 })   ;
+
+      });
+
+
+    // socket.emit('newEmail',{
+    //   from:"Sunil",
+    //   text : "Hello From Sunil",
+    //   createdAt: 12344
+    // });
 
     
+    
+    socket.emit('newMessage',{
+        from:" Sunil",
+        text : "New Message From Sunil",
+        
+      });
+
+    // socket.on('disconnect', function(){
+    //     console.log('User Disconnected');
+    // });
+    
+    
+   });
+
+   
 
 app.use(express.static(publicPath)); 
 
     
 
 
-    app.listen(port);
+    server.listen(port,()=>{
+        console.log(`Server is up and running at ${port}`)
+    });
